2ba11baa8fb877665843159a7b3827f784dfe83c
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:22:13 -0600

    Created friend details component

---
+++ src/app/friends/friend-details/friend-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-details',
+  standalone: false,
+  templateUrl: './friend-details.component.html',
+  styleUrl: './friend-details.component.css'
+})
+export class FriendDetailsComponent {
+
+}
