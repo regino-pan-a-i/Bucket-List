066815ba3a7e387c5c974954ce61a7e4d8261950
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:28:54 -0600

    Created recommendation form component

---
+++ src/app/recommendations/recommendation-form/recommendation-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-form',
+  standalone: false,
+  templateUrl: './recommendation-form.component.html',
+  styleUrl: './recommendation-form.component.css'
+})
+export class RecommendationFormComponent {
+
+}
