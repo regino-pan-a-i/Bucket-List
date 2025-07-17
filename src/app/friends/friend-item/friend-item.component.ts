bb5a7b7aaf50e4f6e76301d8744d2fb958ec4090
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:22:54 -0600

    Created friend item component

---
+++ src/app/friends/friend-item/friend-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-item',
+  standalone: false,
+  templateUrl: './friend-item.component.html',
+  styleUrl: './friend-item.component.css'
+})
+export class FriendItemComponent {
+
+}
