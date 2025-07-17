e5bf6e3f15da0f4b283be2de1a8c67516906c127
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:28:18 -0600

    Created recommendation details component

---
+++ src/app/recommendations/recommendation-details/recommendation-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-details',
+  standalone: false,
+  templateUrl: './recommendation-details.component.html',
+  styleUrl: './recommendation-details.component.css'
+})
+export class RecommendationDetailsComponent {
+
+}
