ce9ba8188981692a02ad6c2937760eb0ee229916
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:29:08 -0600

    Created recommendation item component

---
+++ src/app/recommendations/recommendation-item/recommendation-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommendation-item',
+  standalone: false,
+  templateUrl: './recommendation-item.component.html',
+  styleUrl: './recommendation-item.component.css'
+})
+export class RecommendationItemComponent {
+
+}
