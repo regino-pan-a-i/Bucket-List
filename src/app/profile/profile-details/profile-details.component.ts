dd117e836dd6982f53aaacb6cf56ff8a74bb7bf4
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:27:09 -0600

    Created profile details component

---
+++ src/app/profile/profile-details/profile-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-details',
+  standalone: false,
+  templateUrl: './profile-details.component.html',
+  styleUrl: './profile-details.component.css'
+})
+export class ProfileDetailsComponent {
+
+}
