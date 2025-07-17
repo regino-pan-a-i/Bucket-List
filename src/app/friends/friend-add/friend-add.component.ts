22c4642a9711779e287a2947dde10a92e74f95f6
Andre Regino <reginopanai@gmail.com>
2025-07-16 22:21:08 -0600

    Created friend add component

---
+++ src/app/friends/friend-add/friend-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-add',
+  standalone: false,
+  templateUrl: './friend-add.component.html',
+  styleUrl: './friend-add.component.css'
+})
+export class FriendAddComponent {
+
+}
