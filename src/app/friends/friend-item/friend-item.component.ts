af0337082415769fa69555393e115de35b25bfc6
Andre Regino <reginopanai@gmail.com>
2025-07-23 11:51:22 -0600

    frends recommendation list will now display a only if the user clicks on the see details button

--- src/app/friends/friend-item/friend-item.component.ts
+++ src/app/friends/friend-item/friend-item.component.ts
@@ -11,4 +11,31 @@ export class FriendItemComponent {
 
   @Input()  friend : Friend
 
+  details : boolean = false
+  
+  ngOnInit(){
+    console.log(this.friend.recommendedItems.length)
+    setTimeout(()=>{
+      document.getElementById('recCount'+this.friend.id).innerText = this.friend.recommendedItems.length.toString()
+    }, 500)
+    
+
+  }
+
+  clickDetails(friendId : string){
+    this.details = !this.details
+    const text = document.getElementById('viewDetails' + friendId).innerText 
+    if (text != "Close")
+    {
+      document.getElementById('viewDetails' + friendId).innerText = "Close"
+      document.getElementById('viewDetails' + friendId).classList.remove('btn-info')
+      document.getElementById('viewDetails' + friendId).classList.add('btn-danger')
+      
+    }else{
+      document.getElementById('viewDetails' + friendId).innerText = "See Details"
+      document.getElementById('viewDetails' + friendId).classList.remove('btn-danger')
+      document.getElementById('viewDetails' + friendId).classList.add('btn-info')
+
+    }
+  }
 }
--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FriendService } from './friend.service';
 import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 
 @Component({
@@ -11,7 +13,38 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class FriendsComponent {
 
+  isRouterOutletActive: boolean = false;
+
   constructor(private friendService: FriendService, private router: Router){
       this.friendService.friendSelectedEvent
     }
+
+  ngOnInit(){
+    // Check initial route state
+    this.checkRouterOutletState();
+
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.checkRouterOutletState();
+    });
+  }
+
+  checkRouterOutletState() {
+    const currentUrl = this.router.url;
+    // Check if we're on a child route of recommendations
+    this.isRouterOutletActive = currentUrl.includes('/recommendations/') && currentUrl !== '/recommendations';
+    console.log('Current URL:', currentUrl, 'Router outlet active:', this.isRouterOutletActive);
+  }
+
+  onRouterOutletActivate(component: any) {
+    this.isRouterOutletActive = true;
+    console.log('Router outlet activated');
+  }
+
+  onRouterOutletDeactivate() {
+    this.isRouterOutletActive = false;
+    console.log('Router outlet deactivated');
+  }
 }
